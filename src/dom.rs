74e2fff7a9271b2b0a5f59eefe057d75b6ecd5d6
kgtkr <contact@kgtkr.net>
2023-05-16 18:17:57 +0900

    idom

--- src/dom.rs
+++ src/dom.rs
@@ -54,3 +54,64 @@ fn dom(func: &Func) -> HashMap<BBId, HashSet<BBId>> {
 
     doms
 }
+
+fn idom(func: &Func) -> HashMap<BBId, HashSet<BBId>> {
+    let doms = dom(func);
+    let sdoms = doms
+        .iter()
+        .map(|(a, dom)| {
+            (
+                *a,
+                dom.into_iter().filter(|&b| a != b).collect::<HashSet<_>>(),
+            )
+        })
+        .collect::<HashMap<_, _>>();
+
+    let mut sdom_preds = func
+        .bbs
+        .iter()
+        .map(|bb| (bb.id, HashSet::new()))
+        .collect::<HashMap<_, _>>();
+    for (a, sdom) in &sdoms {
+        for b in sdom {
+            sdom_preds.get_mut(b).unwrap().insert(*a);
+        }
+    }
+
+    let idoms = sdoms
+        .iter()
+        .map(|(a, sdom)| {
+            (*a, {
+                sdom.iter()
+                    .copied()
+                    .filter(|b| sdom_preds[b].iter().all(|c| !sdom.contains(c)))
+                    .copied()
+                    .collect::<HashSet<_>>()
+            })
+        })
+        .collect::<HashMap<_, _>>();
+
+    idoms
+}
+/*
+fn dom_front(func: &Func) -> HashMap<BBId, HashSet<BBId>> {
+    let idoms = idom(func);
+
+    let df_local = func
+        .bbs
+        .iter()
+        .map(|bb| {
+            (bb.id, {
+                bb.term()
+                    .nexts()
+                    .iter()
+                    .filter(|&next| !idoms[&bb.id].contains(next))
+                    .copied()
+                    .collect::<HashSet<_>>()
+            })
+        })
+        .collect::<HashMap<_, _>>();
+
+    ()
+}
+*/
